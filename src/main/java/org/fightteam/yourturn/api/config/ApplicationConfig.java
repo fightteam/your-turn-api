0497daadc303d0656c6c6ed36e3cb80b9d052386
excalibur <excalibur-faith@qq.com>
2014-06-24 19:41:14 +0800

    add mysql support

--- src/main/java/org/fightteam/yourturn/api/config/ApplicationConfig.java
+++ src/main/java/org/fightteam/yourturn/api/config/ApplicationConfig.java
@@ -1,9 +1,14 @@
 package org.fightteam.yourturn.api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 
 /**
  * spring 的javaconfig配置
@@ -15,4 +20,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
         @ComponentScan.Filter(Configuration.class)})
 public class ApplicationConfig {
 
+    @Bean
+    public DataSource dataSource() {
+        Context ctx = new InitialContext();
+        return (DataSource) ctx.lookup("java:jboss/datasources/mySQLDB");
+    }
 }
