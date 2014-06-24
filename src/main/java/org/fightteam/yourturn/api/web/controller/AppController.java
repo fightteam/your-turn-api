7b89465343634ba6b0f04e29dc49bb1d2cabf010
excalibur <excalibur-faith@qq.com>
2014-06-24 17:07:38 +0800

    add AppController junit test

--- src/main/java/org/fightteam/yourturn/api/web/controller/AppController.java
+++ src/main/java/org/fightteam/yourturn/api/web/controller/AppController.java
@@ -3,16 +3,23 @@ package org.fightteam.yourturn.api.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Created by excalibur on 2014/6/23.
+ * 介于API没有UI界面,使用全部以json展现
  */
 @Controller
 public class AppController {
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String index(){
+    public @ResponseBody
+    Map<String, String> index(){
         System.out.println("--------------------");
-        return "index";
+        Map<String, String> json = new HashMap<>();
+        json.put("aa","bbb");
+        return json;
     }
 }
--- src/test/java/org/fightteam/yourturn/api/web/controller/AppControllerTest.java
+++ src/test/java/org/fightteam/yourturn/api/web/controller/AppControllerTest.java
@@ -1,6 +1,7 @@
 package org.fightteam.yourturn.api.web.controller;
 
 import org.fightteam.yourturn.api.test.SpringMvcTest;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
@@ -8,19 +9,15 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 /**
- * Created by excalibur on 2014/6/23.
+ *  AppController测试类
  */
 public class AppControllerTest extends SpringMvcTest {
 
     @Test
     public void testIndex() throws Exception {
         ResultActions ra = this.mockMvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.ALL));
-
-
         MvcResult mr = ra.andReturn();
-        System.out.println(mr);
-        System.out.println(mr.getModelAndView());
         String result = mr.getResponse().getContentAsString();
-        System.out.println(result);
+        Assert.assertEquals("{\"aa\":\"bbb\"}", result);
     }
 }
